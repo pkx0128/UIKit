78ed09cdb899ac340d1389e3d3902c81d85075fe
pankx <764670547@qq.com>
2017-09-17 10:19:37 +0800

    使用UIIamgeView(iamge:,higlightedImage)创建UIImageView

--- MUIImageView/MUIImageView/ViewController.swift
+++ MUIImageView/MUIImageView/ViewController.swift
@@ -29,6 +29,15 @@ class ViewController: UIViewController {
         image1.center = CGPoint(x: view.bounds.width * 0.5, y: view.bounds.height * 0.36)
         view.addSubview(image1)
         
+        //使用UIIamgeView(iamge:,higlightedImage)创建UIImageView
+        let image2 = UIImageView(image: UIImage(named: "test2"), highlightedImage: UIImage(named: "test3"))
+        //设置原点与大小
+        image2.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        //设置位置
+        image2.center = CGPoint(x: view.bounds.width * 0.5, y: view.bounds.height * 0.55)
+        //设置是否为高亮状态
+        image2.isHighlighted = true
+        view.addSubview(image2)
         
     }
 
