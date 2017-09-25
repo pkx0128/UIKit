5f9b11d26fad670574ec29bdd118ab3193a49b6c
pankx <764670547@qq.com>
2017-09-25 11:13:58 +0800

    创建UITableview

---
+++ MUITableView/MUITableView/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  MUITableView
+//
+//  Created by pankx on 2017/9/25.
+//  Copyright © 2017年 pankx. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    //define info
+    let info = [["java","c"],["go",".net","c++","swift","javascript"]]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        //create mytableview
+        let mytableview = UITableView(frame:CGRect(x: 0, y: 0, width: view.bounds.width, height: view.bounds.height),style: .grouped)
+        //register cell
+        mytableview.register(UITableViewCell.self, forCellReuseIdentifier: "myCell")
+        //set delegate and dataSource
+        mytableview.dataSource = self
+        mytableview.delegate = self
+        //add to view
+        view.addSubview(mytableview)
+    }
+    
+    //set Section
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return info.count
+    }
+    
+    //set numberofRowinSection
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return info[section].count
+    }
+    //set cell info
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //define cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+        //set cell.text value
+        cell.textLabel?.text = info[indexPath.section][indexPath.row]
+        return cell
+    }
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
