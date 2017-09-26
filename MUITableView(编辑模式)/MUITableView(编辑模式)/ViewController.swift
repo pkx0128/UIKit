69e8ad382c882a0db2aba8245b8ce69864edb813
pankx <764670547@qq.com>
2017-09-26 12:23:57 +0800

    create tabelview

--- MUITableView(编辑模式)/MUITableView(编辑模式)/ViewController.swift
+++ MUITableView(编辑模式)/MUITableView(编辑模式)/ViewController.swift
@@ -11,10 +11,12 @@ import UIKit
 class ViewController: UIViewController {
 
     let info = ["java","C++","C","PHP","swift","Object-c",".net"]
+    var mytable: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "EditTable"
-        view.backgroundColor = UIColor.white
+        setupUI()
+        setupNav()
+        setupTabel()
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,4 +26,48 @@ class ViewController: UIViewController {
 
 
 }
+extension ViewController {
+    func setupUI() {
+        title = "EditTable"
+        view.backgroundColor = UIColor.white
+    }
+    
+    func setupNav() {
+        navigationController?.navigationBar.barTintColor = UIColor.blue
+        let leftbutton = UIBarButtonItem(barButtonSystemItem: .edit, target: self, action: #selector(setEdit))
+        navigationItem.leftBarButtonItem = leftbutton
+    }
+    //create tableview
+    func setupTabel() {
+        mytable = UITableView(frame: CGRect(x: 0,y: 0,width: view.bounds.width,height: view.bounds.height - 64), style: .plain)
+        mytable.center = view.center
+        mytable.register(UITableViewCell.self, forCellReuseIdentifier: "myCell")
+        mytable.delegate = self
+        mytable.dataSource = self
+        view.addSubview(mytable)
+        
+    }
+    
+    @objc func setEdit() {
+        mytable.isEditing = true
+    }
+    
+}
+//MARK: tableview dalegation method
+extension ViewController : UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return info.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+        cell.textLabel?.text = info[indexPath.row]
+        return cell
+    }
+}
+
+
+
+
+
+
 
