35ae573bf07b6792a29f82be3898236ce162d84e
pankx <764670547@qq.com>
2017-09-28 12:25:38 +0800

    增加界面UI

--- MUserDefaults/MUserDefaults/ViewController.swift
+++ MUserDefaults/MUserDefaults/ViewController.swift
@@ -9,17 +9,61 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var mytext: UITextField!
+    var upButton: UIButton!
+    var delButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setupNav()
+        setupUI()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+   @objc func showMyInfo() {
+    
     }
-
-
 }
+
+extension ViewController {
+    //set nav
+    func setupNav() {
+        title = "Home"
+        let rightbutton = UIBarButtonItem(title: "show", style: .done, target: self, action: #selector(showMyInfo))
+        navigationItem.rightBarButtonItem = rightbutton
+    }
+    func setupUI() {
+        mytext = UITextField(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 40))
+        mytext.center = CGPoint(x: view.bounds.width * 0.5, y: view.bounds.height * 0.3)
+        mytext.textAlignment = .center
+        mytext.placeholder =  "please inuput"
+        view.addSubview(mytext)
+        
+        upButton = UIButton(frame: CGRect(x: 0, y: 0, width: 100, height: 40))
+        upButton.center = view.center
+        upButton.setTitle("UPData", for: .normal)
+        upButton.setTitleColor(UIColor.blue, for: .normal)
+        upButton.addTarget(self, action: #selector(upMethod), for: .touchUpInside)
+        view.addSubview(upButton)
+        
+        delButton = UIButton(frame: CGRect(x: 0, y: 0, width: 100, height: 40))
+        delButton.center = CGPoint(x: view.bounds.width * 0.5, y: view.bounds.height * 0.6)
+        delButton.setTitleColor(UIColor.blue, for: .normal)
+        delButton.setTitle("DelData", for: .normal)
+        delButton.addTarget(self, action: #selector(DelMethod), for: .touchUpInside)
+        view.addSubview(delButton)
+        
+    }
+    
+    @objc func upMethod() {
+        
+    }
+    
+    @objc func DelMethod() {
+        
+    }
+}
+
+
+
+
 
