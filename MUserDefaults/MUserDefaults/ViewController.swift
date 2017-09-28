df5649da8776b5da34e902a35f7026b32d0c808e
pankx <764670547@qq.com>
2017-09-28 11:37:38 +0800

    init project

---
+++ MUserDefaults/MUserDefaults/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MUserDefaults
+//
+//  Created by pankx on 2017/9/28.
+//  Copyright © 2017年 pankx. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
