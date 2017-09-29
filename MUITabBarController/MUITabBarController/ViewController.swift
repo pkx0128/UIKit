334aa884277f3a34079b80eb7309df2b2b922f53
pankx <764670547@qq.com>
2017-09-29 11:07:42 +0800

    init

---
+++ MUITabBarController/MUITabBarController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MUITabBarController
+//
+//  Created by pankx on 2017/9/29.
+//  Copyright © 2017年 pankx. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.cyan
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
