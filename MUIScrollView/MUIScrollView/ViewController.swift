f9e668e8866084bc623a71b65efa97edcc9eef42
pankx <764670547@qq.com>
2017-10-02 11:01:42 +0800

    初始化项目并创建UIScrollView

---
+++ MUIScrollView/MUIScrollView/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  MUIScrollView
+//
+//  Created by pankx on 2017/10/2.
+//  Copyright © 2017年 pankx. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var myScroll: UIScrollView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+extension ViewController {
+    func setupUI() {
+        //创建myScroll
+        myScroll = UIScrollView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: view.bounds.height))
+        myScroll.contentSize = CGSize(width: view.bounds.width * 3, height: view.bounds.height * 2)
+        view.addSubview(myScroll)
+    }
+}
+
