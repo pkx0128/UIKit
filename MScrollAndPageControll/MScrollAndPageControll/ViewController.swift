a426e51461b4e7daf263c3f1af1cbdc5c82e7525
pankx <764670547@qq.com>
2017-10-03 10:48:23 +0800

    创建UIPageController

--- MScrollAndPageControll/MScrollAndPageControll/ViewController.swift
+++ MScrollAndPageControll/MScrollAndPageControll/ViewController.swift
@@ -12,18 +12,32 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor.white
         setupUI()
+        setupPage()
     }
 }
 extension ViewController {
     func setupUI() {
+        view.backgroundColor = UIColor.white
         let myScroll = UIScrollView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: view.bounds.height))
         myScroll.center = view.center
         myScroll.contentSize = CGSize(width: view.bounds.width * 5, height: view.bounds.height)
         myScroll.isPagingEnabled = true
-        myScroll.showsHorizontalScrollIndicator = false
+        myScroll.showsHorizontalScrollIndicator = true
         myScroll.showsVerticalScrollIndicator = false
         view.addSubview(myScroll)
+    }
+    func setupPage() {
+        let myPage = UIPageControl(frame: CGRect(x: 0, y: 0, width: view.bounds.width * 0.85 , height: 50))
+        myPage.center = CGPoint(x: view.bounds.width * 0.5, y: view.bounds.height * 0.9)
+        myPage.numberOfPages = 5
+        myPage.currentPage = 0
+        myPage.pageIndicatorTintColor = UIColor.darkGray
+        myPage.currentPageIndicatorTintColor = UIColor.black
+        myPage.addTarget(self, action:#selector(pagechange) , for: .valueChanged)
+        view.addSubview(myPage)
+    }
+    @objc func pagechange() {
+        
     }
 }
