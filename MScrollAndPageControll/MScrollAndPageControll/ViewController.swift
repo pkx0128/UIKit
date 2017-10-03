9709121ea2da176a46229acd48e2d211e64a4f13
pankx <764670547@qq.com>
2017-10-03 11:30:10 +0800

    给UIScrollView添加5个UILabel

--- MScrollAndPageControll/MScrollAndPageControll/ViewController.swift
+++ MScrollAndPageControll/MScrollAndPageControll/ViewController.swift
@@ -9,24 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var myScroll: UIScrollView!
+    var mylabel: UILabel!
+    var myPage: UIPageControl!
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
         setupPage()
     }
 }
-extension ViewController {
+extension ViewController: UIScrollViewDelegate {
     func setupUI() {
         view.backgroundColor = UIColor.white
-        let myScroll = UIScrollView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: view.bounds.height))
-        myScroll.center = view.center
+        myScroll = UIScrollView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: view.bounds.height))
+        myScroll.center = CGPoint(x: view.bounds.width * 0.5, y: view.bounds.height * 0.5)
         myScroll.contentSize = CGSize(width: view.bounds.width * 5, height: view.bounds.height)
         myScroll.isPagingEnabled = true
         myScroll.showsHorizontalScrollIndicator = true
         myScroll.showsVerticalScrollIndicator = false
+        myScroll.delegate = self
         view.addSubview(myScroll)
+        for i in 0...4 {
+            mylabel = UILabel(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 40))
+            mylabel.center = CGPoint(x: view.bounds.width * (0.5 + CGFloat(i)), y: view.bounds.height * 0.2)
+            mylabel.textAlignment = .center
+            mylabel.text = "\(i + 1)"
+            myScroll.addSubview(mylabel)
+        }
     }
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        print(#function)
+    }
+    
     func setupPage() {
         let myPage = UIPageControl(frame: CGRect(x: 0, y: 0, width: view.bounds.width * 0.85 , height: 50))
         myPage.center = CGPoint(x: view.bounds.width * 0.5, y: view.bounds.height * 0.9)
