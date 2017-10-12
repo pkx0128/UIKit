c92d8c2988f68be3c57b2a166bda703f851f47f6
pankx <764670547@qq.com>
2017-10-12 11:03:04 +0800

    Implement the Rotation

--- MUIRotationGestureRecognizer/MUIRotationGestureRecognizer/ViewController.swift
+++ MUIRotationGestureRecognizer/MUIRotationGestureRecognizer/ViewController.swift
@@ -9,12 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var rView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        rView = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        rView.center = view.center
+        rView.backgroundColor = UIColor.blue
+        view.addSubview(rView)
+        
+        let rotation = UIRotationGestureRecognizer(target: self, action: #selector(rotationEvent))
+        rView.addGestureRecognizer(rotation)
     }
 
+    @objc func rotationEvent(e: UIRotationGestureRecognizer) {
+        rView.transform = CGAffineTransform(rotationAngle: e.rotation)
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
