749aee52c4c0f406bdb733eb3c5ac996a83bf86c
pankx <764670547@qq.com>
2017-10-12 10:43:19 +0800

    UIPinchGestureRecognizer

--- MUIPinchGestureRecognizer/MUIPinchGestureRecognizer/ViewController.swift
+++ MUIPinchGestureRecognizer/MUIPinchGestureRecognizer/ViewController.swift
@@ -9,12 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var pinchView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        pinchView = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        pinchView.center = view.center
+        pinchView.backgroundColor = UIColor.blue
+        view.addSubview(pinchView)
+        
+        let pinchG = UIPinchGestureRecognizer(target: self, action: #selector(pinchEvent))
+        pinchView.addGestureRecognizer(pinchG)
     }
 
+    @objc func pinchEvent(e: UIPinchGestureRecognizer) {
+        let pf = pinchView.frame
+        if e.state == .began {
+            print("pinch Began!")
+        }else if e.state == .changed {
+            if pf.width * e.scale > 100 && pf.height * e.scale < 400 {
+                pinchView.frame = CGRect(x: pf.origin.x, y: pf.origin.y, width: pf.width * e.scale, height: pf.height * e.scale)
+            }
+        }else if e.state == .ended {
+            print("Pinch End")
+        }
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
